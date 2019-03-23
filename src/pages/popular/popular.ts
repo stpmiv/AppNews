07ecef8fd8ff55e77f3c4a921f3fbe04271746d5
Sathaporn Chumwong <mivstp3912@gmail.com>
2019-03-23 13:57:07 +0700

    update4 230362

--- src/pages/popular/popular.ts
+++ src/pages/popular/popular.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { DetailPage} from '../detail/detail';
+
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -31,5 +33,7 @@ export class PopularPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad PopularPage');
   }
-
+  viewDetail(item){
+    this.navCtrl.push(DetailPage,{item:item});
+  }
 }
